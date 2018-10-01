720865ba9da77e8b1f115cd745bab3951d0b0806
cfhammill <cfhammill@gmail.com>
2018-10-01 15:58:24 -0400

    Fix xml links in a more clean way
    Uses the html CDATA module suggested by Yihui
    in rstudio/blogdown#337

--- R/after_build.R
+++
@@ -1,9 +0,0 @@
-`%>%` <- purrr::`%>%`
-
-xmls <- list.files(pattern = ".xml", recursive = TRUE)
-
-purrr::walk(xmls, function(rss){
-  readLines(rss) %>%
-    gsub("/blog/blog", "/blog", .) %>%
-    writeLines(rss)
-})
